87f34bef3eaff36a18dd0f242f53f5d451e9f262
Alisher <81736817+Dastirchiyigit@users.noreply.github.com>
2023-08-10 11:16:05 +0500

    e
    e

--- users/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import CustomUser, Saved
-from django.contrib.auth.models import Group
-
-# Register your models here.
-
-admin.site.unregister(Group)
-admin.site.register(CustomUser)
-admin.site.register(Saved)
